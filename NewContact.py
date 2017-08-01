da58a75675085c5d0b1ebb7fc36c7877fcd4fadd
ksemish <ksemish@gmail.com>
2017-08-01 15:40:35 +0300

    Создание и использование вспомогательного класса для хранения свойств контакта

--- NewContact.py
+++ NewContact.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
+from contact import Contacts
 
 def is_alert_present(wd):
     try:
@@ -14,83 +15,72 @@ class NewContact(unittest.TestCase):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
 
-    def login(self, wd):
+    def login(self, wd, user, passw):
         wd.get("http://localhost/addressbook/index.php")
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(user)
         wd.find_element_by_id("content").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(passw)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def init_new_contact_form(self, wd):
-        # init new contact form
         wd.find_element_by_id("content").click()
         wd.find_element_by_link_text("add new").click()
 
-    def fill_name_contact(self, wd):
-        # fill name of new contact
+    def fill_contact_form(self, wd, Contacts):
+        # fill name of contact
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("User")
+        wd.find_element_by_name("firstname").send_keys(Contacts.firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("Test")
+        wd.find_element_by_name("middlename").send_keys(Contacts.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("New")
+        wd.find_element_by_name("lastname").send_keys(Contacts.lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("New Test User")
+        wd.find_element_by_name("nickname").send_keys(Contacts.nickname)
         wd.find_element_by_name("theform").click()
-
-    def fill_company_information(self, wd):
-        # fill information about company
+        # fill company information
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("New Company")
+        wd.find_element_by_name("company").send_keys(Contacts.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("Address new company")
-
-    def fill_contact_information(self, wd):
+        wd.find_element_by_name("address").send_keys(Contacts.address_new_company)
         # fill contact information
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("12345")
+        wd.find_element_by_name("home").send_keys(Contacts.home)
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("4567")
+        wd.find_element_by_name("mobile").send_keys(Contacts.mobile)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys("789")
+        wd.find_element_by_name("work").send_keys(Contacts.work)
         wd.find_element_by_name("fax").click()
         wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys("234")
-
-    def fill_email_information(self, wd):
+        wd.find_element_by_name("fax").send_keys(Contacts.fax)
         # fill email of contact
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("one@gmail.com")
+        wd.find_element_by_name("email").send_keys(Contacts.email1)
         wd.find_element_by_name("email2").click()
         wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys("two@gmail.com")
+        wd.find_element_by_name("email2").send_keys(Contacts.email2)
         wd.find_element_by_name("email3").click()
         wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys("three@gmail.com")
-
-    def fill_homepage(self, wd):
+        wd.find_element_by_name("email3").send_keys(Contacts.email3)
         # fill homepage
         wd.find_element_by_name("homepage").click()
         wd.find_element_by_name("homepage").clear()
         wd.find_element_by_name("homepage").send_keys("homepage.ru")
-
-    def fill_dates_contact(self, wd):
-        # fill birhday and aniversary date
+        # fill birhday date
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[4]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[4]").click()
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[6]").is_selected():
@@ -98,6 +88,7 @@ class NewContact(unittest.TestCase):
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys("1990")
+        # fill aniversary date
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[7]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[7]").click()
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[4]").is_selected():
@@ -105,44 +96,33 @@ class NewContact(unittest.TestCase):
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
         wd.find_element_by_name("ayear").send_keys("2013")
-
-    def fill_home_information(self, wd):
-        # fill home address and phone
+        # fill homeaddress
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys("secondary address")
+        wd.find_element_by_name("address2").send_keys(Contacts.homeaddress)
+        # fill homephone
         wd.find_element_by_name("phone2").click()
         wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys("home address")
+        wd.find_element_by_name("phone2").send_keys(Contacts.homephone)
+        # fill comments
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys("comments")
-
-    def submit_create_contact(self, wd):
-        # subbmit contact creation
+        wd.find_element_by_name("notes").send_keys(Contacts.notes)
+        # submit_create_contact
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
     def return_main_page(self, wd):
-        # return to main page
         wd.find_element_by_xpath("//div/div[4]/div/i/a[2]").click()
 
     def logout(self, wd):
-        # logout
         wd.find_element_by_link_text("Logout").click()
 
     def test_NewContact(self):
         success = True
         wd = self.wd
-        self.login(wd)
+        self.login(wd, "admin", "secret")
         self.init_new_contact_form(wd)
-        self.fill_name_contact(wd)
-        self.fill_company_information(wd)
-        self.fill_contact_information(wd)
-        self.fill_email_information(wd)
-        self.fill_homepage(wd)
-        self.fill_dates_contact(wd)
-        self.fill_home_information(wd)
-        self.submit_create_contact(wd)
+        self.fill_contact_form(wd, Contacts("User", "Test", "New", "New Test User", "New Company", "Address new company", "12345", "4567", "789", "234", "one@gmail.com", "two@gmail.com", "three@gmail.com", "secondary address", "home address", "comments"))
         self.return_main_page(wd)
         self.logout(wd)
 
---
+++ contact.py
@@ -0,0 +1,18 @@
+class Contacts:
+    def __init__(self, firstname, middlename, lastname, nickname, company, address_new_company, home, mobile, work, fax, email1, email2,email3, homeaddress, homephone, notes):
+       self.firstname = firstname
+       self. middlename = middlename
+       self.lastname = lastname
+       self.nickname = nickname
+       self.company = company
+       self.address_new_company = address_new_company
+       self.home = home
+       self.mobile = mobile
+       self.work = work
+       self.fax = fax
+       self.email1 = email1
+       self.email2 = email2
+       self.email3 = email3
+       self.homeaddress = homeaddress
+       self.homephone = homephone
+       self.notes = notes
